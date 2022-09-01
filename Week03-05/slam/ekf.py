d550b053c8fc50edc0b3c3b337484a7f291c1104
Yew Siang Ku <ykuu0003@student.monash.edu>
2022-09-01 19:27:32 +1000

    updated ekf.py (update function) :)

--- Week03-05/slam/ekf.py
+++ Week03-05/slam/ekf.py
@@ -115,10 +115,10 @@ class EKF:
         
         if (np.absolute(x[0]) < 0.01 and np.absolute(x[1]) < 0.01 and np.absolute(x[2]) < 0.1): #The robot is within 0.01 unit square of the origin
             for i in range(len(measurements)):
-                R[2*i:2*i+2,2*i:2*i+2] = measurements[i].covariance
+                R[2*i:2*i+2,2*i:2*i+2] = 0
         else:
             for i in range(len(measurements)):
-                R[2*i:2*i+2,2*i:2*i+2] = 0
+                R[2*i:2*i+2,2*i:2*i+2] = measurements[i].covariance
 
         # Compute own measurements
         z_hat = self.robot.measure(self.markers, idx_list)
